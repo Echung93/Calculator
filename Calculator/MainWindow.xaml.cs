c281e699ffdefc5208c580edfd3d4e8b74c4f98c
충훈 이 <kty309@gmail.com>
2021-11-28 10:14:44 +0900

    2021_11_28

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -20,9 +20,20 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        string number;
+        string number1;
+        string number2;
+        string function;
+        string function1 = null;
+        double n;
+        double n1;
+        double resultNum;
+
+
         public MainWindow()
         {
             InitializeComponent();
+
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
@@ -30,12 +41,305 @@ namespace Calculator
 
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void num_Click(object sender, RoutedEventArgs e)
         {
             Button button = sender as Button;
-            string number = button.Content.ToString();  // 현재 입력한 버튼 숫자
-            //MessageBox.Show($"{button.Name} 클릭 되었습니다.");
-            string number1 = button.Name.ToString();
+            function1 = null;
+            number = button.Content.ToString();  // 현재 입력한 버튼 숫자          
+            if (text.Text == "0")
+            {
+                text.Text = number;
+            }
+
+            else
+            {
+                text.Text += number;
+                number1 += number;
+            }
+
+            if (number1 == "\0")
+            {
+                if (number1.Contains("."))
+                {
+                    n = double.Parse(number1);
+                }
+
+                else
+                {
+                    n = double.Parse(text.Text);
+                    text.Text = string.Format("{0:N0}", n);
+                }
+            }
+
+        }
+        private void negative_Click(object sender, RoutedEventArgs e)
+        {
+            if (text.Text != null)
+            {
+                n1 = double.Parse(text.Text) * (-1);
+                text.Text = n1.ToString();
+                number1 = text.Text;
+            }
+        }
+
+        private void point_Click(object sender, RoutedEventArgs e)
+        {
+            if (!text.Text.Contains("."))
+                text.Text += ".";
+        }
+
+        private void equals_Click(object sender, RoutedEventArgs e)
+        {
+            if (function1 == null)
+            {
+                switch (function)
+                {
+                    case "+":
+                        if (number1 == null)
+                        {
+                            n = double.Parse(number2);
+                            n1 = double.Parse(number2);
+                            resultNum = n1 + n;
+                        }
+
+                        else
+                        {
+                            n = double.Parse(number1);
+                            n1 = double.Parse(number2);
+                            resultNum = n1 + n;
+                        }
+
+                        number2 = resultNum.ToString();
+                        text.Text = resultNum.ToString();
+                        function1 = "+";
+                        break;
+
+                    case "-":
+                        if (number1 == null)
+                        {
+                            n = double.Parse(number2);
+                            n1 = double.Parse(number2);
+                            resultNum = n1 - n;
+                        }
+
+                        else
+                        {
+                            n = double.Parse(number1);
+                            n1 = double.Parse(number2);
+                            resultNum = n1 - n;
+                        }
+
+                        number2 = resultNum.ToString();
+                        text.Text = resultNum.ToString();
+                        function1 = "-";
+                        break;
+
+                    case "×":
+                        if (number1 == null)
+                        {
+                            n = double.Parse(number2);
+                            n1 = double.Parse(number2);
+                            resultNum = n1 * n;
+                        }
+
+                        else
+                        {
+                            n = double.Parse(number1);
+                            n1 = double.Parse(number2);
+                            resultNum = n1 * n;
+                        }
+
+                        number2 = resultNum.ToString();
+                        text.Text = resultNum.ToString();
+                        function1 = "×";
+                        break;
+
+                    case "÷":
+                        if (number1 == null)
+                        {
+                            n = double.Parse(number2);
+                            n1 = double.Parse(number2);
+                            resultNum = n1 / n;
+                        }
+
+                        else
+                        {
+                            n = double.Parse(number1);
+                            n1 = double.Parse(number2);
+                            resultNum = n1 / n;
+                        }
+
+                        number2 = resultNum.ToString();
+                        text.Text = resultNum.ToString();
+                        function1 = "÷";
+                        break;
+                }
+            }
+
+            else
+            {
+                switch (function)
+                {
+                    case "+":
+                        if (number2 == text.Text)
+                        {
+                            n1 = double.Parse(number2);
+                            n1 += n;
+                            number2 = n1.ToString();
+                        }
+                        text.Text = number2;
+                        break;
+
+                    case "-":
+                        if (number2 == text.Text)
+                        {
+                            n1 = double.Parse(number2);
+                            n1 -= n;
+                            number2 = n1.ToString();
+                        }
+                        text.Text = number2;
+                        break;
+
+                    case "×":
+                        if (number2 == text.Text)
+                        {
+                            n1 = double.Parse(number2);
+                            n1 *= n;
+                            number2 = n1.ToString();
+                        }
+                        text.Text = number2;
+                        break;
+
+                    case "÷":
+                        if (number2 == text.Text)
+                        {
+                            n1 = double.Parse(number2);
+                            n1 /= n;
+                            number2 = n1.ToString();
+                        }
+                        text.Text = number2;
+                        break;
+                }
+            }
+
+
+        }
+
+        private void plus_Click(object sender, RoutedEventArgs e)
+        {
+            function = "+";
+            if (function1 == null)
+            {
+                number2 = text.Text;
+                text.Text += "+";
+            }
+
+            else
+            {
+                n = double.Parse(number2);
+                function1 = "+";
+                text.Text += "+";
+            }
+
+            number1 = null;
+        }
+
+        private void subtract_Click(object sender, RoutedEventArgs e)
+        {
+            function = "-";
+            if (function1 == null)
+            {
+                number2 = text.Text;
+                text.Text += "-";
+            }
+
+            else
+            {
+                n = double.Parse(number2);
+                function1 = "-";
+                text.Text += "-";
+            }
+
+            number1 = null;
+        }
+
+        private void multiply_Click(object sender, RoutedEventArgs e)
+        {
+            function = "×";
+            if (function1 == null)
+            {
+                number2 = text.Text;
+                text.Text += "*";
+            }
+
+            else
+            {
+                n = double.Parse(number2);
+                text.Text += "*";
+                function1 = "×";
+            }
+
+            number1 = null;
+        }
+
+        private void dividing_Click(object sender, RoutedEventArgs e)
+        {
+            function = "÷";
+
+            if (function1 == null)
+            {
+                number2 = text.Text;
+                text.Text += "÷";
+            }
+
+            else
+            {
+                n = double.Parse(number2);
+                text.Text += "÷";
+                function1 = "÷";
+            }
+
+            number1 = null;
+        }
+
+        private void clear_Click(object sender, RoutedEventArgs e)
+        {
+            if (text.Text.Length == 0)
+            {
+
+            }
+            else
+            {
+                text.Text = text.Text.Substring(0, (text.Text.Length - 1));
+                if(number1 != null)
+                    number1 = number1.Substring(0, (number1.Length - 1));
+            }
+        }
+
+        private void per_Click(object sender, RoutedEventArgs e)
+        {
+            if (number1 != null)
+            {
+                n1 = double.Parse(number1) / 100;
+                number2 = n1.ToString();
+                text.Text = number2;
+                number1 = null;
+            }
+
+        }
+
+        private void reset_Click(object sender, RoutedEventArgs e)
+        {
+            number = "0";
+            number1 = "0";
+            number2 = "0";
+            text.Text = "";
+            function = null;
+            function1 = null;
+            n = 0;
+            n1 = 0;
+            resultNum = 0;
+
         }
     }
 }
